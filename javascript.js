83b2153ec5fa3c8cd351430304aa9b7cd1cf62fa
simonesciacovelli <scias758@newschool.edu>
2019-08-23 21:29:45 +0200

    es

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
+
+
 $(document).ready(function(){
-	$(".iframe").css("background-color", "yellow");
-	$('iframe[src*="embed.spotify.com"]').css("background-color", "yellow");
+	$(".frame").hover(function(){
+		$(".container2").css('background','black');
+		// $(".container2").css('width','90%');
+		// $(".mejs__container").css('background','black');
+		// $(".mejs__container").css('opacity','10');
+  	},
+
+
+  function(){
+    $(".container2").css('background',' #3c4044');
+    $(".container2").css('width','60%');
+    // $(".mejs__container").css('opacity','0');
+  });
 });
+
