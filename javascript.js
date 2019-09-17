f0c113c294f18b648170b669e74b5cd464e50f68
simonesciacovelli <scias758@newschool.edu>
2019-09-17 16:08:51 -0400

    sd

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
 
 
-$(document).ready(function(){
-	$(".frame").hover(function(){
-		$(".container2").css('background','black');
-		// $(".container2").css('width','90%');
-		// $(".mejs__container").css('background','black');
-		// $(".mejs__container").css('opacity','10');
-  	},
+// $(document).ready(function(){
+// 	$(".frame").hover(function(){
+// 		$(".container2").css('background','black');
+// 		// $(".container2").css('width','90%');
+// 		// $(".mejs__container").css('background','black');
+// 		// $(".mejs__container").css('opacity','10');
+//   	},
 
 
-  function(){
-    $(".container2").css('background',' #3c4044');
-    $(".container2").css('width','60%');
-    // $(".mejs__container").css('opacity','0');
-  });
-});
+//   function(){
+//     $(".container2").css('background',' #3c4044');
+//     $(".container2").css('width','60%');
+//     // $(".mejs__container").css('opacity','0');
+//   });
+// });
 
